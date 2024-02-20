6a1bdc6f62c8be1d345f9714dea46ff89c6a0769
lisa <eamazitova@edu.hse.ru>
2024-02-20 20:24:54 +0300

    fixed

--- cpp-lab-10.cpp
+++ cpp-lab-10.cpp
@@ -1,14 +1,19 @@
 ﻿#include <iostream>
+// добавить приоритет +
+// исправить метод push с учетом приоритета  +
+// деструктор добавить (чтобы удалял все Node)  +
 using namespace std;
 template <typename T>
 class MyPriorityQueue {
 protected:
     struct Node {
+        int priority;
         T data;
         Node* next;
-        Node(T data, Node* next) {
+        Node(T data, Node* next, int priority) {
             this->data = data;
             this->next = next;
+            this->priority = priority;
         }
     };
     Node* head;
@@ -16,17 +21,22 @@ public:
     MyPriorityQueue() {
         head = nullptr;
     }
-    void push(T data) {
-        Node* Node1 = new Node(data, nullptr);
-        if (head == nullptr) {
-            head = Node1;
+    void push(T data, int priority) {
+        Node* Nodepushed = new Node(data, nullptr, priority);
+        if (head == nullptr || head->priority > Nodepushed->priority) {
+            if (head == nullptr ) head = Nodepushed;
+            else {
+                Nodepushed->next = head;
+                head = Nodepushed;
+            }
         }
         else {
             Node* now = head;
-            while (now->next != nullptr) {
-                now = now->next;  //до конца очереди
+            while (now->next != nullptr && now->next->priority <= Nodepushed->priority) {
+                now = now->next;
             }
-            now->next = Node1;  // новый элемент после последнего узла
+            if (now->next == nullptr) now->next = Nodepushed;
+            else { Nodepushed->next = now->next; now->next = Nodepushed; }
         }
     }
     void pop() {
@@ -57,38 +67,45 @@ public:
         }
         return cnt;
     }
-    friend ostream& operator << (ostream& ostream, MyPriorityQueue queue) {
+    friend ostream& operator << (ostream& ostream, const MyPriorityQueue& queue) {
         if (queue.head != nullptr) {
             ostream << "Head element of the queue: " << queue.head->data << endl;
         }
         ostream << "Size of the queue: " << queue.size() << endl;
-        ostream << "All elements of the queue: ";
+        ostream << "All elements of the queue: " << endl;
         Node* now = queue.head;
         while (now != nullptr) {
-            ostream << now->data << " ";
+            ostream << "Element: " << now->data << ", priority: " << now->priority << endl;
             now = now->next;
         }
         ostream << endl;
         return ostream;
+    }
+    ~MyPriorityQueue() {
+        Node* now = head;
+        Node* next;
+        while (now != nullptr) {
+            next = now->next;
+            delete now;
+            now = next;
+        }
+        head = nullptr;
     }
 };
 int main() {
     char data1 = 'a';
     char data2 = 'b';
     char data3 = 'c';
+    char data4 = 'd';
     MyPriorityQueue <char> queue;
-    queue.push(data1);
-    queue.push(data2);
-    queue.push(data3);
+    queue.push(data1, 1);
+    queue.push(data2, 3);
+    queue.push(data3, 1);
+    queue.push(data4, 1);
     cout << "Queue size: " << queue.size() << endl;
     cout << queue.peek() << endl;
     queue.pop();
     cout << queue.peek() << endl;
     cout << "-----------------------" << endl;
     cout << queue;
-    cout << "-----------------------" << endl;
-    MyPriorityQueue<int> queue2;
-    queue2.pop();
-    queue2.peek();
-    cout << queue2;
 }
